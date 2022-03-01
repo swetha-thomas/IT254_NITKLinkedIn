6ec5ced8fe30a1e539680ea42d100f5242891981
Swetha Thomas <78926897+swetha-thomas@users.noreply.github.com>
2022-03-02 00:39:10 +0400

    Add files via upload

---
+++ static/javascript/edit_profile.js
@@ -0,0 +1,59 @@
+function myFunction_experience() {
+    var x = document.getElementById("experience");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y=document.getElementById("expbutton").innerHTML="Hide My Work Experience"
+    } else {
+      x.style.display = "none";
+      y=document.getElementById("expbutton").innerHTML="Show My Work Experience"
+
+    }
+  }
+
+  function myFunction_club() {
+    var x = document.getElementById("club");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y=document.getElementById("clubbutton").innerHTML="Hide My Clubs"
+    } else {
+      x.style.display = "none";
+      y=document.getElementById("clubbutton").innerHTML="Show My Clubs"
+
+    }
+  }
+
+  function myFunction_edu() {
+    var x = document.getElementById("edu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y=document.getElementById("edubutton").innerHTML="Hide My Education"
+    } else {
+      x.style.display = "none";
+      y=document.getElementById("edubutton").innerHTML="Show My Education"
+
+    }
+  }
+
+  function myFunction_courses() {
+    var x = document.getElementById("course");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y=document.getElementById("coursebutton").innerHTML="Hide My Courses and Certifications"
+    } else {
+      x.style.display = "none";
+      y=document.getElementById("coursebutton").innerHTML="Show My Courses and Certifications"
+
+    }
+  }
+
+  function myFunction_skills() {
+    var x = document.getElementById("skill");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y=document.getElementById("skillbutton").innerHTML="Hide My Skills"
+    } else {
+      x.style.display = "none";
+      y=document.getElementById("skillbutton").innerHTML="Show My Skills"
+
+    }
+  }
