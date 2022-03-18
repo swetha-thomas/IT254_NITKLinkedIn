f7024617c4e4df4ae157206102ecfdc1392d7296
Swetha Thomas <swethat1410@gmail.com>
2022-03-18 13:29:23 +0400

    Partial Update on Login

--- static/javascript/login_page.js
+++ static/javascript/login_page.js
@@ -1,46 +1,36 @@
-var student = document.getElementById("flexRadioDefault1");
-var organiser = document.getElementById("flexRadioDefault2");
-
-
-// student.addEventListener('click', function (params) {
-//     document.getElementById("exampleInputEmail1").pattern = "^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$";
-// });
-
-// organiser.addEventListener('click', function() {
-//     document.getElementById("exampleInputEmail1").pattern = "^[a-z][a-z]{2,}@[a-z]{3,}\.org";
-// })
+var student = document.getElementById("student");
+var organization = document.getElementById("organization");
 
 function validateInput() {
     var email, regex, password;
-    if(student.checked == "true") {
-        email = document.getElementById("exampleInputEmail1");
-        regex = /^[a-z][a-z]+\.\d{3}[a-z]{2}\d{3}@nitk\.edu\.in$/;
+    if(student!=null) {
+        email = document.getElementById("email");
+        regex = /^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$/;
         if(!regex.test(email.value)) {
             return false;
         }
     }
     else {
-        email = document.getElementById("exampleInputEmail1");
-        regex = /^[a-z][a-z]{2,}@[a-z]{3,}\.org/
+        email = document.getElementById("email");
+        regex = /^[a-zA-Z0-9][a-zA-Z0-9\._\-]*@[a-zA-Z0-9\-\.]+\.[a-z]+$/
         if(!regex.test(email.value)) {
             return false;
         }        
     }
-    password = document.getElementById("exampleInputPassword1");
-    // regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])(?=.{8,20})/
-    // regex = /^(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    regex = /^[a-z][a-z]+\.\d{3}[a-z]{2}\d{3}@nitk\.edu\.in$/;
-    if(!regex.test(password.value)) {
-        return false;
-    }
-    return true;
+   
 }
-var signinBtn = document.getElementById("signin");
-signinBtn.addEventListener('click', () => {
-    if(validateInput()) {
-        if(student.checked)
-            signinBtn.onclick = "location.href = 'www.youtube.com';";
+function signInButton()
+{
+        if(student)
+        {
+            console.log("hi")
+
+            location.replace = '../templates/student_home.html';
+        }
         else
-            signinBtn.onclick = "organization_home.html";
-    }
-})
+        {
+        console.log("hi")
+            location.replace = '../templates/organization_home.html';
+        }
+    
+}
