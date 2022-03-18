f2085e2b05d58a5602bbe2f451ea087a19ac0911
Swetha Thomas <swethat1410@gmail.com>
2022-03-18 11:04:24 +0400

    Updated CSS and JS

--- static/javascript/login_page.js
+++ static/javascript/login_page.js
@@ -1,10 +1,10 @@
-var student = document.getElementById("flexRadioDefault1");
-var organiser = document.getElementById("flexRadioDefault2");
+var student = document.getElementById("student");
+var organiser = document.getElementById("organizer");
 
 student.addEventListener('click', function (params) {
-    document.getElementById("exampleInputEmail1").pattern = "^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$";
+    document.getElementById("email").pattern = "^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$";
 });
 
 organiser.addEventListener('click', function() {
-    document.getElementById("exampleInputEmail1").pattern = "^[a-z][a-z]{2,}@[a-z]{3,}\.org";
+    document.getElementById("eamil").pattern = "^[a-z][a-z]{2,}@[a-z]{3,}\.org";
 })
