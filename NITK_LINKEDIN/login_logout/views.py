dcc7406a498ec7f5834015fece7b5152f4bc170b
Advaith <advaithcurpod@gmail.com>
2022-04-26 12:28:15 +0530

    username stuff added in register and login pages

--- NITK_LINKEDIN/login_logout/views.py
+++ NITK_LINKEDIN/login_logout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as login_user
+from django import forms
 
 # from .models import CustomUser
 # from student.models import Student
@@ -10,7 +11,25 @@ temp_name = ""
 
 
 # Create your views here.
-
+# def check_login_regex(user, email, password, username):
+#   # organisation email regex no need
+  
+#   if(user== 'student'):
+#     if(re.search("^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$", email) is None):
+#       print("email regex does not match")
+#       return False
+    
+#   # password and username regex check is same for org and student
+#   if(re.search("^[a-zA-Z]([_-](?![_-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", username) is None):
+#     print("username regex doesnot match")
+#     return False
+  
+#   if(re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})", password) is None):
+#       print("password regex doesnt match")
+#       return False
+  
+#   return True
+  
 def login(request):
   if request.method == 'POST':
     if request.POST.get('flexRadioDefault') == 'student':
@@ -24,7 +43,8 @@ def login(request):
         messages.info(request, "Invalid Credentials")
         return redirect('login')
     else:
-      return redirect('organizationHome')
+      print("regex doesnt match")
+    
   return render(request, 'login_page.html')
 
 def logout(request):
@@ -34,6 +54,7 @@ def logout(request):
 def register(request):
   if request.method == 'POST':
     if request.POST.get('register_type') == 'student':
+      username = request.POST['username'].strip().lower()
       fullName = request.POST["fullName"].strip()
       email = request.POST["email"]
       password = request.POST["password"]
--- NITK_LINKEDIN/static/javascript/register_page.js
+++ NITK_LINKEDIN/static/javascript/register_page.js
@@ -1,53 +1,54 @@
-var role_student = document.querySelector("#student");
-var role_organiser = document.querySelector("#organiser");
+var role_student = document.getElementById("student");
+var role_organiser = document.getElementById("organiser");
 
-var username = document.getElementById("fullName");
+var full_name = document.getElementById("fullName");
 var email = document.getElementById("email");
+var username_div = document.getElementsByClassName("username_class");
 
 role_organiser.addEventListener("click", displayOrganiserFields);
 role_student.addEventListener("click", displayStudentFields);
 
 function displayOrganiserFields() {
-    username.placeholder = "Name of organisation";
+    username_div[0].style.display = "none";
 
+    full_name.placeholder = "Name of organisation";
     email.placeholder = "";
-    email.pattern = "^[a-zA-Z0-9][a-zA-Z0-9\._\-]*@[a-zA-Z0-9\-\.]+\.[a-z]+$";
 }
 
 function displayStudentFields() {
-    
-    username.placeholder = "FirstName LastName";
-    username.pattern = "^[A-Z][a-z]* [A-Z][a-z]*}$";
+    username_div[0].style.display = "block";
+
+    username.placeholder = "username";
+    full_name.placeholder = "FirstName LastName";
     email.placeholder = "your_name.roll_no@nitk.edu.in";
-    email.pattern = "^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$";
 }
 
-function validate() {
-    var regex_name = /^[A-Z][a-z]* [A-Z][a-z]*$/
-    if(!regex_name.test(username.value)) {
-        return false;
-    }
+// function validate() {
+//     var regex_name = /^[A-Z][a-z]* [A-Z][a-z]*$/
+//     if(!regex_name.test(full_name.value)) {
+//         return false;
+//     }
 
-    var regex_email;
-    if (role_student.checked) {
-        regex_email = /^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$/;
-    } else {
-        regex_email = /^[a-zA-Z0-9][a-zA-Z0-9\._\-]*@[a-zA-Z0-9\-\.]+\.[a-z]+$/;
-    }
-    if(!regex_email.test(email.value)) {
-        return false;
-    }
+//     var regex_email;
+//     if (role_student.checked) {
+//         regex_email = /^[a-z][a-z]+\.[0-9]{3}[a-z]{2}[0-9]{3}@nitk\.edu\.in$/;
+//     } else {
+//         regex_email = /^[a-zA-Z0-9][a-zA-Z0-9\._\-]*@[a-zA-Z0-9\-\.]+\.[a-z]+$/;
+//     }
+//     if(!regex_email.test(email.value)) {
+//         return false;
+//     }
 
-    var regex_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})/;
-    var password = document.getElementById("password");
-    if(!regex_password.test(password.value)) {
-        return false;
-    }
+//     var regex_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})/;
+//     var password = document.getElementById("password");
+//     if(!regex_password.test(password.value)) {
+//         return false;
+//     }
 
-    var confirmPassword = document.getElementById("conf_password");
-    if(confirmPassword.value != password.value) {
-        return false;
-    }
-    return true;
-    
-}
+//     var confirmPassword = document.getElementById("conf_password");
+//     if(confirmPassword.value != password.value) {
+//         return false;
+//     }
+//     return true;
+
+// }
