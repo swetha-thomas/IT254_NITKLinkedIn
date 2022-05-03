8afac7c51c89e001d7f9399ea7e59a99651cf392
not-shoyo <akheelachu@gmail.com>
2022-05-03 14:37:39 +0530

    images now work :D

--- NITK_LINKEDIN/NITK_LINKEDIN/urls.py
+++ NITK_LINKEDIN/NITK_LINKEDIN/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from NITK_LINKEDIN import settings
+from django.contrib.staticfiles.urls import static
+
 urlpatterns = [
     path('', include('login_logout.urls')),
     # path('student/job/', include('job.urls')),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('organization/', include('organization.urls')),
     path('admin/', admin.site.urls),
     path('student/job/', include('job.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
