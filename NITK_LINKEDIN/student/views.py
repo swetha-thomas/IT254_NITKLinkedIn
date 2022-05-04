6c6484b21dfced34acfb6d2937c607b3ebcd8cb1
Advaith <advaithcurpod@gmail.com>
2022-05-04 12:52:19 +0530

    small formatting issue fixed

--- NITK_LINKEDIN/student/views.py
+++ NITK_LINKEDIN/student/views.py
@@ -23,6 +23,7 @@ def studentJob(request):
   
   experience_level = form.get('experience_level')
   company = form.get('company')
+  # print(company)
   job_type = form.get('job_type')
   onsite_remote = form.get('onsite_remote')
   
@@ -35,7 +36,7 @@ def studentJob(request):
     # print()
     
     # onsite_remote has to be added to models.py of job because currently its is just location_of_work, so cannot match with that
-    if (experience_level is None or job.job_level in experience_level) and (company is None or job.company in company) and (job_type is None or job.job_type in job_type) and (onsite_remote is None or job.onsite_remote in onsite_remote):
+    if ((experience_level is None or job.job_level in experience_level) and (company is None or job.company in company) and (job_type is None or job.job_type in job_type) and (onsite_remote is None or job.onsite_remote in onsite_remote)):
       # print(job)
       # print(job.company)
       filtered_jobs.add(job)
